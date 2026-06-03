46d795297c9ab37b13e5c7bca3aa373ecdbd9159
aidenjastrzembski <aidenjastrzembski@gmail.com>
2026-06-03 00:18:22 -0400

    add go chi router

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	http.ListenAndServe(":3000", r)
 }
